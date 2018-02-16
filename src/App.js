0ebf53bf3b154bed5d8522c4289f812f98650b1f
avdhoot <avdhoot.980@gmail.com>
2018-02-16 22:53:29 +0530

    some minor cleanup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-let defaultTextColor = 'black';
+
 
 let defaultStyle = {
-  color: defaultTextColor
+  color: 'black'
 };
 
 
@@ -51,7 +50,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Title</h1>
+        <h1 style = {{...defaultStyle,'font-size': '54px'  }} >Title</h1>
         <Aggregate/>
         <Aggregate/>
         <Filter/>
