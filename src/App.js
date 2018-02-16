d3f898c420bc18dc37fd0f02bdc15ca6794be5a0
avdhoot <avdhoot.980@gmail.com>
2018-02-16 22:38:08 +0530

    first commit
    just basic component design

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+let defaultTextColor = 'black';
+
+let defaultStyle = {
+  color: defaultTextColor
+};
+
+
+class Aggregate extends Component{
+    render () {
+      return (
+          <div style = {{...defaultStyle,width: "40%" , display: 'inline-block'}}> 
+             <h2>Number text</h2>
+          </div>
+      );
+    }
+}
+
+
+class Filter extends Component{
+  render () {
+    return (
+      <div style={defaultStyle} >
+        <img/>
+        <input type="text"/>
+      </div>
+    );
+  }
+}
+
+
+
+class Playlist extends Component{
+  render () {
+    return (
+      <div style = {{...defaultStyle,display: 'inline-block', width: "25%"}} >
+        <img/>
+        <h3>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
+      </div>
+    );
+  }
+}
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Title</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+export default App;
