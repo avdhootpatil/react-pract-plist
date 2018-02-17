c9f6de84480fa01a346d31cb0650bd508e3ae6d5
avdhoot <avdhoot.980@gmail.com>
2018-02-17 16:43:34 +0530

    additional feature of calculating playlists and hours after filtering

--- src/App.js
+++ src/App.js
@@ -117,6 +117,11 @@ class App extends Component {
   }
 
   render() {
+    let playlistToRender  = this.state.serverData.user ?
+     this.state.serverData.user.playlists.filter(playlist =>
+      playlist.name.toLowerCase().includes(
+        this.state.filterString.toLowerCase())
+    ) : []
     return (
       <div className="App">
         {this.state.serverData.user ?
@@ -124,15 +129,13 @@ class App extends Component {
             <h1 style = {{...defaultStyle,'font-size': '54px'  }} >
               {this.state.serverData.user.name}'s Playlists
             </h1>
-            <PlaylistCounter playlists={ this.state.serverData.user.playlists}/>
-            <HoursCounter playlists={ this.state.serverData.user.playlists}/>
+            <PlaylistCounter playlists={playlistToRender}/>
+            <HoursCounter playlists={ playlistToRender}/>
             <Filter onTextChange = {text =>{
                   this.setState({filterString: text})
             }}/>
             {
-              this.state.serverData.user.playlists.filter(playlist =>
-                playlist.name.toLowerCase().includes(this.state.filterString.toLowerCase())
-              ).map(playlists =>
+              playlistToRender.map(playlists =>
                 <Playlist playlist = {playlists}/>
               )
             }
